29e12568c3dd5f826cf6e72a6794d0bc7921fa99
Michal Miszczyszyn <michal@mmiszy.pl>
2018-07-01 15:52:46 +0200

    Add tslint

--- test.config.ts
+++ test.config.ts
@@ -1,6 +1,6 @@
 import 'symbol-observable';
 
 import { configure } from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
+import * as Adapter from 'enzyme-adapter-react-16';
 
 configure({ adapter: new Adapter() });
