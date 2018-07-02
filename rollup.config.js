772c7ee1ea16a23af4a5df5433132c5671a16f36
Michal Miszczyszyn <michal@mmiszy.pl>
2018-07-03 00:58:34 +0200

    Change bundle format

--- rollup.config.js
+++ rollup.config.js
@@ -5,14 +5,25 @@ import typescript from 'rollup-plugin-typescript2';
 import pkg from './package.json';
 
 export default [
-  // browser-friendly UMD build
   {
     input: 'src/index.tsx',
-    output: {
-      name: 'react-with-observable',
-      file: pkg.browser,
-      format: 'umd',
-    },
+    output: [
+      {
+        name: 'react-with-observable',
+        file: pkg.browser,
+        format: 'umd',
+      },
+      {
+        name: 'react-with-observable',
+        file: pkg.module,
+        format: 'es',
+      },
+      {
+        name: 'react-with-observable',
+        file: pkg.main,
+        format: 'cjs',
+      },
+    ],
     plugins: [
       resolve(),
       commonjs({
@@ -22,16 +33,4 @@ export default [
     ],
     external: ['react', 'react-dom', 'create-subscription'],
   },
-
-  // CommonJS (for Node) and ES module (for bundlers) build.
-  // (We could have three entries in the configuration array
-  // instead of two, but it's quicker to generate multiple
-  // builds from a single configuration where possible, using
-  // an array for the `output` option, where we can specify
-  // `file` and `format` for each target)
-  // {
-  //   input: 'src/main.js',
-  //   external: ['ms'],
-  //   output: [{ file: pkg.main, format: 'cjs' }, { file: pkg.module, format: 'es' }],
-  // },
 ];
